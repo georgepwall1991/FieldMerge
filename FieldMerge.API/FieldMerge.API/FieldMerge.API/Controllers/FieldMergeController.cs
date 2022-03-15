b8d145586886e9aa3672dd17345117d795251230
George Wall <georgewall1991@icloud.com>
2022-03-15 21:25:31 +0000

    registering service + creating controller

---
+++ FieldMerge.API/FieldMerge.API/FieldMerge.API/Controllers/FieldMergeController.cs
@@ -0,0 +1,27 @@
+﻿using FieldMerge.API.Interfaces;
+using FieldMerge.Data.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FieldMerge.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FieldMergeController : ControllerBase
+{
+    private readonly IFieldMergeService _fieldMergeService;
+
+    public FieldMergeController(IFieldMergeService fieldMergeService)
+    {
+        _fieldMergeService = fieldMergeService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<FieldCodeConversionPattern>>> GetAllPatterns()
+    {
+        var fieldConversionPatterns = await _fieldMergeService.LoadFieldCodeConversionPatterns();
+        if (fieldConversionPatterns.Any())
+            return Ok(fieldConversionPatterns);
+
+        return BadRequest("No patterns available");
+    }
+}
--- FieldMerge.API/FieldMerge.API/FieldMerge.API/Program.cs
+++ FieldMerge.API/FieldMerge.API/FieldMerge.API/Program.cs
@@ -1,14 +1,20 @@
+using FieldMerge.API.Interfaces;
+using FieldMerge.API.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IFieldMergeService, FieldMergeService>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
