0c0b25bcc77c7f6fc0494ad1563e1279f4f818b3
George Wall <84231788+georgepwall1991@users.noreply.github.com>
2022-03-15 21:26:25 +0000

    tidy up
    Signed-off-by: George Wall <georgewall1991@icloud.com>

--- FieldMerge.API/FieldMerge.API/FieldMerge.API/Program.cs
+++ FieldMerge.API/FieldMerge.API/FieldMerge.API/Program.cs
@@ -14,15 +14,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
